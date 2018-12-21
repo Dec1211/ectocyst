128db9dd3a7ad7de6e6b18d630d31876ba611f41
Dec1211 <471986249@qq.com>
2018-12-21 20:32:24 +0800

    项目搭建

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  created () {
+    this.$http('/apis/test', {
+      params: {
+        fild: 1,
+        fild2: 2,
+        file3: 3
+      }
+    }).then(function (response) {
+      console.log(response)
+    }).catch(function (error) {
+      console.log(error)
+    })
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  font-size: 0.02rem;
+}
+</style>
